74b7f882dab8d6198f011d42b7dd5d62a11d2cbf
Christopher Barnes <christopher@barnes.biz>
2025-02-25 11:06:40 +0100

    Add sourcing after copying configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 ln -s ./shell/.zshrc ~/.zshrc
 ln -s ./.gitconfig ~/.gitconfig
 
+source ~/.zshrc
+
 ./brew/setup_brew.sh
