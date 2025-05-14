e5e57ef957f4d1b3715cabe4785d1610a5edf47b
Christopher Barnes <christopher@barnes.biz>
2025-05-14 21:45:54 +0200

    Add git setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 ln -s $HOME/Code/dotfiles/shell/.zshrc ~/.zshrc
 ln -s $HOME/Code/dotfiles/shell/.gitconfig ~/.gitconfig
 
+cp ./.gitconfig.cern ~/.gitconfig.cern
+cp ./.gitconfig.global ~/.gitconfig.global
+
 source ~/.zshrc
 
 ./brew/setup_brew.sh
